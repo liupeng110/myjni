fa6c854c146868e16e07c2ba3f1fa0a45ec46a4d
liupeng110 <717219917@qq.com>
2017-03-09 16:00:29 +0800

    0.0

--- app/src/main/java/com/andlp/myjni/MainActivity.java
+++ app/src/main/java/com/andlp/myjni/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import com.andlp.myjni.jni.JniUtil;
 import com.getkeepsafe.relinker.ReLinker;
 
+import cn.dxjia.ffmpeg.library.FFmpegNativeHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView txt; Button btn; EditText edit;
@@ -29,9 +31,20 @@ public class MainActivity extends AppCompatActivity {
     private void initData(){
         ReLinker.loadLibrary(MainActivity.this, "hellojni");
         txt.setText(JniUtil.test());
+        String gif = "ffmpeg -i /sdcard/DCIM/1.mp4 -vframes 30 -y -f gif /sdcard/abc.gif";
+        edit.setText(gif);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               String common=edit.getText().toString();
+                if (common==null||common.equals("")){
+                String result= FFmpegNativeHelper.runCommand("ffmpeg -version");
+                  txt.setText(result);
+                }else{
+                    String result=  FFmpegNativeHelper.runCommand(common);
+                    txt.setText(result);
+                }
+
 
             }
         });
