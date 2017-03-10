c6da9e75a7274476063a321344ca049340dfe309
liupeng110 <717219917@qq.com>
2017-03-10 11:04:47 +0800

    添加lib

---
+++ app/src/main/java/com/andlp/myjni/MyApp.java
@@ -0,0 +1,17 @@
+package com.andlp.myjni;
+
+import com.andlib.lp.AndApp;
+import com.andlib.lp.util.L;
+
+/**
+ * 717219917@qq.com  2017/3/10 11:01
+ */
+
+public class MyApp extends AndApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        L.i("应用切入点0");
+    }
+}
